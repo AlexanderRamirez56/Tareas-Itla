ddf98ee5cbcc5027c3f16e82ea66a0240a98adb1
Alexander <papa7@DESKTOP-13J92SK>
2025-04-27 19:19:18 -0400

    Add project files.

---
+++ Ejercicio 1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class ProgramaDivision
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Ingrese el primer número (dividendo): ");
+        double dividendo = Convert.ToDouble(Console.ReadLine());
+
+
+        Console.Write("Ingrese el segundo número (divisor): ");
+        double divisor = Convert.ToDouble(Console.ReadLine());
+
+
+        if (divisor == 0)
+        {
+            Console.WriteLine("Error: No se puede dividir entre cero.");
+        }
+        else
+        {
+
+            double resultado = dividendo / divisor;
+            Console.WriteLine($"El resultado de dividir {dividendo} entre {divisor} es: {resultado}");
+        }
+
+
+        Console.WriteLine("Presione cualquier tecla para salir...");
+        Console.ReadKey();
+    }
+}
