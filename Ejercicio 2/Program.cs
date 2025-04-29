6e6416f364fd10e0a23fcfe003c52b34df287f16
Alexander <papa7@DESKTOP-13J92SK>
2025-04-29 09:22:49 -0400

    Agregar Ejercicio 2

---
+++ Ejercicio 2/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        Console.Write("Ingrese el primer número: ");
+        double numero1 = Convert.ToDouble(Console.ReadLine());
+
+        
+        Console.Write("Ingrese el segundo número: ");
+        double numero2 = Convert.ToDouble(Console.ReadLine());
+
+        
+        double resultado = numero1 * numero2;
+
+        
+        Console.WriteLine($"El resultado de la multiplicación es: {resultado}");
+
+        
+        Console.WriteLine("Presione cualquier tecla para salir...");
+        Console.ReadKey();
+    }
+}
